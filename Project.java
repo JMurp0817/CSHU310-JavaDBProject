c12c5c70104a6d612cec9e1b199372670a380fdb
JMurp0817 <jmurp0817@gmail.com>
2020-04-28 02:13:08 -0600

    fix and add ItemsAvailable
    I could've found out what the meaning of life was if I used the same amount of brainpower it took to figure out the query for items available.

--- Project.java
+++ Project.java
@@ -33,8 +33,6 @@ public class Project {
 			ResultSet resultSet = null;
 			int updateResultSet = 0;
 			
-			System.out.println(args[0]+" "+ args[1]);
-			
 			if(args[0] == "/?") {
 				usage();
 			} else if(args[0].equals("CreateItem")) {
@@ -47,34 +45,54 @@ public class Project {
 				if(args.length!=3) {
 					usage();
 				} else {
-					updateResultSet = updateStmt.executeUpdate("INSERT INTO `"+dbName+"`.`Purchase` (ItemID, Quantity) SELECT ID, "+args[2]+" FROM `"+dbName+"`.`Item` WHERE `ItemCode` = "+args[1]+";");
+					updateResultSet = updateStmt.executeUpdate("INSERT INTO `"+dbName+"`.`Purchase` (ItemID, Quantity) SELECT ID, "+args[2]+" FROM `"+dbName+"`.`Item` WHERE `ItemCode` = '"+args[1]+"'");
 				}
 			} else if(args[0].equals("CreateShipment")) {
 				if(args.length!=4) {
 					usage();
 				} else {
-					updateResultSet = updateStmt.executeUpdate("INSERT INTO `"+dbName+"`.`Shipment` (ItemID, Quantity, ShipmentDate) SELECT ID, "+args[2]+", '"+args[3]+"' FROM `"+dbName+"`.`Item` WHERE `ItemCode` = "+args[1]+";");
+					updateResultSet = updateStmt.executeUpdate("INSERT INTO `"+dbName+"`.`Shipment` (ItemID, Quantity, ShipmentDate) SELECT ID, "+args[2]+", '"+args[3]+"' FROM `"+dbName+"`.`Item` WHERE `ItemCode` = '"+args[1]+"'");
 				}
 			} else if(args[0].equals("GetItems")) {
-				if(args[1].equals("%")) {
-					resultSet = queryStmt.executeQuery("SELECT ItemCode,ItemDescription,Price FROM `"+dbName+"`.`Item`");
+				if(args.length != 2) {
+					usage();
 				} else {
-					resultSet = queryStmt.executeQuery("SELECT ItemCode,ItemDescription,Price FROM `"+dbName+"`.`Item` WHERE `ItemCode`='"+args[1]+"'");
+					if(args[1].equals("%")) {
+						resultSet = queryStmt.executeQuery("SELECT ItemCode,ItemDescription,Price FROM `"+dbName+"`.`Item`");
+					} else {
+						resultSet = queryStmt.executeQuery("SELECT ItemCode,ItemDescription,Price FROM `"+dbName+"`.`Item` WHERE `ItemCode`='"+args[1]+"'");
+					}
 				}
 			} else if(args[0].equals("GetShipments")) {
-				if(args[1].equals("%")) {
-					resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Shipment` JOIN `"+dbName+"`.`Item` ON `Shipment`.`ItemID` = `Item`.`ID`");
+				if(args.length != 2) {
+					usage();
 				} else {
-					resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Shipment` JOIN `"+dbName+"`.`Item` ON `Shipment`.`ItemID` = `Item`.`ID` WHERE ItemCode='"+args[1]+"'");
+					if(args[1].equals("%")) {
+						resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Shipment` JOIN `"+dbName+"`.`Item` ON `Shipment`.`ItemID` = `Item`.`ID`");
+					} else {
+						resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Shipment` JOIN `"+dbName+"`.`Item` ON `Shipment`.`ItemID` = `Item`.`ID` WHERE ItemCode='"+args[1]+"'");
+					}
 				}
 			} else if(args[0].equals("GetPurchases")) {
-				if(args[1].equals("%")) {
-					resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Purchase` JOIN `"+dbName+"`.`Item` ON `Purchase`.`ItemID` = `Item`.`ID`");
+				if(args.length != 2) {
+					usage();
 				} else {
-					resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Purchase` JOIN `"+dbName+"`.`Item` ON `Purchase`.`ItemID` = `Item`.`ID` WHERE ItemCode='"+args[1]+"'");
+					if(args[1].equals("%")) {
+						resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Purchase` JOIN `"+dbName+"`.`Item` ON `Purchase`.`ItemID` = `Item`.`ID`");
+					} else {
+						resultSet = queryStmt.executeQuery("SELECT * FROM `"+dbName+"`.`Purchase` JOIN `"+dbName+"`.`Item` ON `Purchase`.`ItemID` = `Item`.`ID` WHERE ItemCode='"+args[1]+"'");
+					}
 				}
 			} else if(args[0].equals("ItemsAvailable")) {
-				
+				if(args.length != 2) {
+					usage();
+				} else {
+					if(args[1].equals("%")) {
+						resultSet = queryStmt.executeQuery("SELECT i.ItemCode, i.ItemDescription, COALESCE(s.ssum,0) - COALESCE(p.psum,0) as ItemsAvailable FROM `"+dbName+"`.Item i LEFT JOIN (SELECT sum(Purchase.Quantity) as psum,Purchase.ItemID FROM `"+dbName+"`.`Purchase` GROUP BY Purchase.ItemID) p ON p.ItemID = i.ID LEFT JOIN (SELECT sum(Shipment.Quantity) as ssum,Shipment.ItemID FROM `"+dbName+"`.`Shipment` GROUP BY  Shipment.ItemID) s ON s.ItemID=i.ID GROUP BY i.ItemCode");
+					} else {
+						resultSet = queryStmt.executeQuery("SELECT i.ItemCode, i.ItemDescription, COALESCE(s.ssum,0) - COALESCE(p.psum,0) as ItemsAvailable FROM `"+dbName+"`.Item i LEFT JOIN (SELECT sum(Purchase.Quantity) as psum,Purchase.ItemID FROM `"+dbName+"`.`Purchase` GROUP BY Purchase.ItemID) p ON p.ItemID = i.ID LEFT JOIN (SELECT sum(Shipment.Quantity) as ssum,Shipment.ItemID FROM `"+dbName+"`.`Shipment` GROUP BY  Shipment.ItemID) s ON s.ItemID=i.ID WHERE i.ItemCode = '"+args[1]+"' GROUP BY i.ItemCode");
+					}
+				}
 			} else if(args[0].equals("UpdateItem")) {
 				if(args.length!=3){
 					usage();
@@ -82,26 +100,38 @@ public class Project {
 					updateResultSet = updateStmt.executeUpdate("UPDATE `"+dbName+"`.`Item` SET Price = "+args[2]+" WHERE ItemCode = "+args[1]+";");
 				}
 			} else if(args[0].equals("DeleteItem")) {
-				updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Item` WHERE ItemCode ='"+args[1]+"'");
-				if(updateResultSet == 1) {
-					System.out.println("Successfully deleted "+args[1]);
+				if(args.length != 2) {
+					usage();
 				} else {
-					System.out.println("There is no Item with the code "+args[1]);
+					updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Item` WHERE ItemCode ='"+args[1]+"'");
+					if(updateResultSet == 1) {
+						System.out.println("Successfully deleted "+args[1]);
+					} else {
+						System.out.println("There is no Item with the code "+args[1]);
+					}
 				}
 			} else if(args[0].equals("DeleteShipment")) {
-				updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Shipment` WHERE ItemID IN (SELECT ID FROM `"+dbName+"`.`Item` WHERE ItemCode = '"+args[1]+"')  ORDER BY ShipmentDate DESC LIMIT 1");
-				if(updateResultSet == 1) {
-					System.out.println("Successfully deleted most recent Shipment of "+args[1]);
+				if(args.length != 2) {
+					usage();
 				} else {
-					System.out.println("There are no Shipments of "+args[1]);
-				}				
+					updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Shipment` WHERE ItemID IN (SELECT ID FROM `"+dbName+"`.`Item` WHERE ItemCode = '"+args[1]+"')  ORDER BY ShipmentDate DESC LIMIT 1");
+					if(updateResultSet == 1) {
+						System.out.println("Successfully deleted most recent Shipment of "+args[1]);
+					} else {
+						System.out.println("There are no Shipments of "+args[1]);
+					}	
+				}
 			} else if(args[0].equals("DeletePurchase")) {
-				updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Purchase` WHERE ItemID IN (SELECT ID FROM `"+dbName+"`.`Item` WHERE ItemCode = '"+args[1]+"')  ORDER BY PurchaseDate DESC LIMIT 1");
-				if(updateResultSet == 1) {
-					System.out.println("Successfully deleted most recent Purchase of "+args[1]);
+				if(args.length != 2) {
+					usage();
 				} else {
-					System.out.println("There are no Purchases of "+args[1]);
-				}	
+					updateResultSet = updateStmt.executeUpdate("DELETE FROM `"+dbName+"`.`Purchase` WHERE ItemID IN (SELECT ID FROM `"+dbName+"`.`Item` WHERE ItemCode = '"+args[1]+"')  ORDER BY PurchaseDate DESC LIMIT 1");
+					if(updateResultSet == 1) {
+						System.out.println("Successfully deleted most recent Purchase of "+args[1]);
+					} else {
+						System.out.println("There are no Purchases of "+args[1]);
+					}	
+				}
 			} else {
 				usage();
 			}
